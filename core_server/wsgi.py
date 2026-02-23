d24fa16fd0e6f35acea22a3b7d94a09932b70aa1
Gisha Shava <192929268+Spearman9453@users.noreply.github.com>
2026-02-23 23:36:33 +0200

    WSGI file update

--- core_server/wsgi.py
+++ core_server/wsgi.py
@@ -1,12 +1,22 @@
 import os
 import sys
+from pathlib import Path
 
-# הגדרת הנתיב לתיקיית הפרויקט
-path = '/home/meydan/access_site'
+# הגדרת נתיב הפרויקט
+path = '/home/GishaShava'
 if path not in sys.path:
     sys.path.append(path)
 
-# הגדרת מודול ה-Settings
+# טעינת משתני סביבה מה-.env
+env_path = Path(path) / '.env'
+if env_path.exists():
+    with env_path.open() as f:
+        for line in f:
+            if line.startswith('export '):
+                key, value = line.replace('export ', '', 1).strip().split('=', 1)
+                os.environ[key.strip()] = value.strip().strip("'").strip('"')
+
+# הגדרת קובץ ההגדרות של דג'נגו
 os.environ['DJANGO_SETTINGS_MODULE'] = 'core_server.settings'
 
 # הפעלת האפליקציה
